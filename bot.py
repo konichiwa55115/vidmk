271f98fde7f6f435506775dd15afd5917de89e16
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-28 18:09:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,60 +54,15 @@ def _telegram_file(client, message):
   picture = "./picy/pic"
   global tempmp3
   tempmp3 = "mod"+mp3file
-
-  global res 
   cmd(f'ffmpeg -i {file_path} -vf arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/{tempmp3} -y')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/{mp3file} -y ')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -vf arnndn=m=./rnnoise-models/beguiling-drafter-2018-08-30/bd.rnnn ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/conjoined-burgers-2018-08-28/cb.rnnn ./downloads/{mp3file} -y ')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -vf arnndn=m=./rnnoise-models/leavened-quisling-2018-08-31/lq.rnnn ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/marathon-prescription-2018-08-29/mp.rnnn ./downloads/{mp3file} -y ')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -af volume=2 ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -r 1 -loop 1 -y -i {picture} -i ./downloads/{tempmp3} -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 {mp4file}')
+  cmd(f'ffmpeg -i ./downloads/{tempmp3} -af volume=2 ./downloads/{mp3file} -y ')
+  cmd(f'ffmpeg -r 1 -loop 1 -y -i {picture} -i ./downloads/{mp3file} -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 {mp4file}')
 
-    # Upload transcription file to user
   with open(f'{mp4file}', 'rb') as f:
         bot.send_video(message.chat.id, f)
   shutil.rmtree('./downloads/')
   shutil.rmtree('./picy/')
   cmd(f'rm {mp4file}')
-
-
-  
-
-@bot.on_message(filters.private & filters.incoming & filters.video )
-def _telegram_file(client, message):
-  if os.path.isdir("./downloads/") :
-        sent_message = message.reply_text('هناك عملية يتم الآن . أرسل التصميم  بعد مدة من فضلك', quote=True)
-        return
-  else :
-        pass
-  
-  user_id = message.from_user.id
-  sent_message = message.reply_text('[جار منتجة الفيديو', quote=True)
-  file = message.audio
-  global file_path
-  file_path = message.download(file_name="aud")
-  global mp3file
-  mp3file = "mp3file.mp3"
-  global mp4file
-  mp4file="mp4file.mp4" 
-  tempmp3 = "mod"+mp3file
-  cmd(f'ffmpeg -i {file_path} -q:a 0 -map a ./downloads/{mp3file} -y')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -af volume=15dB ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/{mp3file} -y ')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -vf arnndn=m=./rnnoise-models/beguiling-drafter-2018-08-30/bd.rnnn ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/conjoined-burgers-2018-08-28/cb.rnnn ./downloads/{mp3file} -y ')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -vf arnndn=m=./rnnoise-models/leavened-quisling-2018-08-31/lq.rnnn ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/marathon-prescription-2018-08-29/mp.rnnn ./downloads/{mp3file} -y ')
-  cmd(f'ffmpeg -i {file_path} -i ./downloads/{mp3file} -c:v copy -map 0:v:0 -map 1:a:0 {mp4file} -y')
-  with open(f'{mp4file}', 'rb') as f:
-        bot.send_video(message.chat.id, f)
-  shutil.rmtree('./downloads/')
-  cmd(f'rm {mp4file}')
-
-
-
 
 
 bot.run()
