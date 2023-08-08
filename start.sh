3961a53c12aa7cea88ff6d260926611208ff9e9f
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-08-08 11:12:48 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
-cd /LazyDeveloper/vidmk
+echo "Cloning Repo...."
+git clone https://github.com/konichiwa55115/vidmk /kony
+cd /kony
+pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
